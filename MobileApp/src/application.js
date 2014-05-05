315e33ce7e5b36d4941ab1a4060db5ba63948851
Manish Sathe <johnsathe@gmail.com>
2014-05-05 20:02:14 +0530

    First view added

---
+++ MobileApp/src/application.js
@@ -0,0 +1,16 @@
+﻿var qzineApp = angular.module('qzineApp', [
+  'ngRoute',
+  'homeControllers'
+]);
+qzineApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'partials/home.html',
+            controller: 'Home'
+        }).
+
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
---
+++ MobileApp/src/controllers/main.js
@@ -0,0 +1,17 @@
+﻿var homeControllers = angular.module('homeControllers', []);
+
+homeControllers.controller('Home', ['$scope', '$http',
+  function ($scope, $http) {
+      /*
+      $http.get('phones/phones.json').success(function (data) {
+          $scope.phones = data;
+      });
+
+      $scope.orderProp = 'age';
+      */
+  }]);
+
+homeControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function ($scope, $routeParams) {
+      $scope.phoneId = $routeParams.phoneId;
+  }]);
